5f94cf41ba7600011e34615c8e089a7017fe33c7
Kunal Vaswani <kvaswani2012@gmail.com>
2020-06-18 16:49:44 +0530

    deleting old version

--- src/redux/index.js
+++
@@ -1,2 +0,0 @@
-export { logIn, logOut, image } from "./user/userAction";
-export { search, recent, modal } from "./search/searchAction";
--- src/redux/search/searchTypes.js
+++
@@ -1,3 +0,0 @@
-export const SEARCH = 'SEARCH';
-export const RECENT = 'RECENT';
-export const MODAL = 'MODAL';
